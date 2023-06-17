3adb66986629f7fe239267f7b80c2af28f40bd3c
Nateemaru <nateemaru@gmail.com>
2023-06-17 17:53:33 +0400

    save

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -1,8 +1,11 @@
 using System;
 using _Scripts.AI.BodyParts;
 using _Scripts.CodeSugar;
+using _Scripts.Services;
 using _Scripts.SO;
 using DG.Tweening;
+using Sirenix.OdinInspector;
+using TrailsFX;
 using UnityEngine;
 
 namespace _Scripts
@@ -10,6 +13,11 @@ namespace _Scripts
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private ProjectileConfig _config;
+        [SerializeField] private TrailEffect _trail;
+
+        [SerializeField] private bool _hasHitEffect;
+        [ShowIf("_hasHitEffect")] [SerializeField] private PoolObjectConfig _hitVfxEffect;
+        
         private Vector3 _direction;
         private bool _isHit;
 
@@ -26,11 +34,13 @@ namespace _Scripts
 
         private void OnTriggerEnter(Collider other)
         {
+            _trail.enabled = false;
             _isHit = true;
             transform.GetComponent<Collider>().enabled = false;
             
             if(other.TryGetComponent(out BodyPart bodyPart))
             {
+                PoolHub.Instance.GetObject(_hitVfxEffect).transform.position = transform.position;
                 bodyPart.Punch(other.transform.position - transform.forward);
                 transform.parent = other.transform;
             }
