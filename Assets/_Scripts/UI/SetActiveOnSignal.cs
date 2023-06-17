63b55f327de7e316d0143855df6caeca105da8bd
Nateemaru <nateemaru@gmail.com>
2023-06-17 18:33:51 +0400

    save

---
+++ Assets/_Scripts/UI/SetActiveOnSignal.cs
@@ -0,0 +1,62 @@
+using _Scripts.Services.EventBusService;
+using _Scripts.Services.EventBusService.EventsInterfaces;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class SetActiveOnSignal : MonoBehaviour, IGameWinSubscriber, IGameLoseSubscriber, 
+        IGamePauseSubscriber, IGameResumeSubscriber
+    {
+        [SerializeField] private bool _toggleToActive = true;
+        
+        [SerializeField] private GameObject[] _onRunning;
+        [SerializeField] private GameObject[] _onWin;
+        [SerializeField] private GameObject[] _onLose;
+        [SerializeField] private GameObject[] _onGamePause;
+
+        private void OnEnable()
+        {
+            EventBus.Subscribe(this);
+        }
+
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe(this);
+        }
+
+        private void UpdateActivity(GameObject[] parent)
+        {
+            if (parent.IsNullOrEmpty()) 
+                return;
+            
+            foreach (GameObject o in parent)
+            {
+                if (o == null)
+                    continue;
+                
+                o.SetActive(_toggleToActive);
+            }
+        }
+
+        public void OnGameWon()
+        {
+            UpdateActivity(_onWin);
+        }
+
+        public void OnGameLost()
+        {
+            UpdateActivity(_onLose);
+        }
+
+        public void OnGamePaused()
+        {
+            UpdateActivity(_onGamePause);
+        }
+
+        public void OnGameResumed()
+        {
+            UpdateActivity(_onRunning);
+        }
+    }
+}
