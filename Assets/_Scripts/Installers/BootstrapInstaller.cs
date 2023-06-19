16fe1bc30bc38a4242b9528b54c49d1f0e729cbf
Nateemaru <nateemaru@gmail.com>
2023-06-19 13:05:39 +0400

    save

--- Assets/_Scripts/Installers/BootstrapInstaller.cs
+++ Assets/_Scripts/Installers/BootstrapInstaller.cs
@@ -1,5 +1,7 @@
 using _Scripts.Services;
+using _Scripts.Services.AudioSystem;
 using _Scripts.Services.CoroutineRunnerService;
+using _Scripts.Services.Database;
 using _Scripts.Services.SceneLoadService;
 using Zenject;
 
@@ -12,6 +14,36 @@ namespace _Scripts.Installers
             BindFPSUnlocker();
             BindCoroutineStarter();
             BindISceneLoadService();
+            BindStorage();
+            BindDataContainer();
+            BindAudioController();
+        }
+
+        private void BindAudioController()
+        {
+            Container
+                .Bind<AudioController>()
+                .AsSingle()
+                .NonLazy();
+        }
+
+        private void BindStorage()
+        {
+            Container
+            .Bind<IStorageService>()
+            .To<JsonStorage>()
+            .AsSingle()
+            .NonLazy();
+        }
+
+        private void BindDataContainer()
+        {
+            Container
+            .Bind<IDataContainer>()
+            .To<GameDataContainer>()
+            .FromNew()
+            .AsSingle()
+            .NonLazy();
         }
 
         private void BindISceneLoadService()
--- Assets/_Scripts/Services/Database/GlobalConstants.cs
+++ Assets/_Scripts/Services/Database/GlobalConstants.cs
@@ -2,13 +2,8 @@ namespace _Scripts.Services.Database
 {
     public static class GlobalConstants
     {
-        public const string MONEY_KEY = "Money";
-        public const string LEVEL_KEY = "Level";
-        public const string PLAYER_STATS_KEY = "PlayerStats";
-        public const string PLAYER_SKIN_ID_KEY = "PlayerSkin";
         public const string VOLUME_KEY = "Volume";
-        public const string RUNNING_SNAPSHOT = "OnRunning";
-        public const string PAUSE_SNAPSHOT = "OnPause";
-        public const string BOUGHT_SKINS_KEY = "BoughtSkins";
+        public const string RUNNING_SNAPSHOT = "Run";
+        public const string PAUSE_SNAPSHOT = "Pause";
     }
 }
